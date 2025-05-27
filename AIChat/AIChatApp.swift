598cea6d2d85588e4fc5a90169d97862212b4383
Saliha Yılmaz <saliha@unknown4e239f4a7138.home>
2025-05-27 15:23:01 +0300

    İlk commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by Saliha Yılmaz on 27.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
